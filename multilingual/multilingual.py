a12d65cdf0c8c2f24789faab6834d23965853c53
Tiger <tiger.dingsun@gmail.com>
2020-02-17 01:07:08 -0500

    debug

--- multilingual/multilingual.py
+++ multilingual/multilingual.py
@@ -85,11 +85,11 @@ def test(model, test_loader, experiment, hyperparams, bpe):
             num_words_in_batch = torch.sum(batch['dec_input_lengths']).item()
             total_loss += loss.item()*num_words_in_batch
             word_count += num_words_in_batch
-            print(torch.max(y_pred, 1).shape)
-            print(torch.max(y_pred, 1))
+            print(torch.max(y_pred, 1)[0].shape)
+            print(torch.max(y_pred, 1)[0])
             print(y_actual.shape)
             print(y_actual)
-            diff = torch.max(y_pred, 1) - y_actual
+            diff = torch.max(y_pred, 1)[0] - y_actual
             num_wrong = np.count_nonzero(diff)
             total_wrong += num_wrong
         perplexity = np.exp(total_loss / word_count)

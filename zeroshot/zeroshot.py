7579a89b439fc1e844cd48f22a2dd03244fab1fd
Tiger <tiger.dingsun@gmail.com>
2020-02-25 16:33:34 -0500

    hyperparameter changes

--- zeroshot/zeroshot.py
+++ zeroshot/zeroshot.py
@@ -11,9 +11,9 @@ from tqdm import tqdm  # optional progress bar
 
 # TODO: Set hyperparameters
 hyperparams = {
-    "rnn_size": 64,  # assuming encoder and decoder use the same rnn_size
-    "embedding_size": 64,
-    "num_epochs": 3,
+    "rnn_size": 32,  # assuming encoder and decoder use the same rnn_size
+    "embedding_size": 32,
+    "num_epochs": 1,
     "batch_size": 20,
     "learning_rate": 0.001
 }

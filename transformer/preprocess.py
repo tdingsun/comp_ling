b034835a03c1fc72fccee08a78782c3d9eed198f
Tiger <tiger.dingsun@gmail.com>
2020-02-29 19:13:40 -0500

    first pass at transformer model

---
+++ transformer/preprocess.py
@@ -0,0 +1,99 @@
+from torch.utils.data import Dataset
+from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data import DataLoader
+import torch
+import numpy as np
+import unicodedata
+import argparse
+import re
+
+PAD_TOKEN = "*PAD*"
+START_TOKEN = "*START*"
+
+
+def load_dataset(train_fn, test_fn, batch_size):
+    """
+    :param fn: filename for the dataset
+    :return: (torch.utils.data.DataLoader, torch.utils.data.DataLoader) for train and test
+    :Comment: You don't have to shuffle the test dataset
+    """
+    word2id = {PAD_TOKEN: 0, START_TOKEN: 1}
+    curr_id = 2
+
+    train_lines = read_from_corpus(train_fn)
+    test_lines = read_from_corpus(test_fn)
+    
+    max_train_len = len(max(train_lines, key = lambda i: len(i)))
+    max_test_len = len(max(test_lines, key = lambda i: len(i)))
+    max_seq_len = max(max_train_len, max_test_len)
+    print("Window Size", max_seq_len)
+
+    train_dataset = TransformerDataset(train_lines, word2id, curr_id, max_seq_len)
+    test_dataset = TransformerDataset(test_lines, train_dataset.word2id, train_dataset.curr_id, max_seq_len)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    
+    return(train_loader, test_loader, len(test_dataset.word2id), max_seq_len)
+
+
+class TransformerDataset(Dataset):
+    def __init__(self, input_lines, word2id, curr_id, max_seq_len):
+        """
+        Read and parse the file line by line.
+        Create a word2id vocab dict.
+        Vectorize all data. 
+        """
+        self.input_vectors = []
+        self.label_vectors = []
+        self.lengths = []
+        self.word2id = word2id
+        self.curr_id = curr_id
+        self.max_seq_len = max_seq_len
+        self.vocab_size = 0
+
+        for line in input_lines:
+            label = line.strip().split()
+
+            label_vector = []
+            for word in label:
+                if word not in self.word2id:
+                    self.word2id[word] = self.curr_id
+                    self.curr_id += 1
+                label_vector.append(self.word2id[word])
+            input_vector = [self.word2id[START_TOKEN]] + label_vector[:-1]
+
+            self.lengths.append(len(label))
+            self.label_vectors.append(torch.tensor(label_vector))
+            self.input_vectors.append(torch.tensor(input_vector))
+        
+        first_pad = torch.zeros(max_seq_len - len(self.input_vectors[0]), dtype=torch.long)
+        self.input_vectors[0] = torch.cat((self.input_vectors[0], first_pad))
+        self.label_vectors[0] = torch.cat((self.label_vectors[0], first_pad))
+
+        self.input_vectors = pad_sequence(self.input_vectors, batch_first=True)
+        self.label_vectors = pad_sequence(self.label_vectors, batch_first=True)
+
+        self.lengths = torch.tensor(self.lengths)
+        self.vocab_size = len(self.word2id)
+
+    def __len__(self):
+        return len(self.input_vectors)
+    
+    def __getitem__(self, idx):
+        item = {
+            "input_vectors": self.input_vectors[idx],
+            "label_vectors": self.label_vectors[idx],
+            "lengths": self.lengths[idx]
+        }
+        return item
+    
+        
+def read_from_corpus(corpus_file):
+    lines = []
+
+    with open(corpus_file, 'r') as f:
+        for line in f:
+            seq = line.strip().split()
+            lines.append(seq)
+    return lines
---
+++ transformer/transformer.py
@@ -0,0 +1,141 @@
+from comet_ml import Experiment
+from preprocess import *
+from model import Transformer
+from torch.utils.data import DataLoader, random_split
+from torch import nn, Optimize
+
+import torch
+import numpy as np
+import argparse
+from tqdm import tqdm
+
+hyper_params = {
+    "batch_size": 100,
+    "num_epochs": 3,
+    "learning_rate": 0.01,
+    "embedding_size": 64
+}
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+
+experiment = Experiment(project_name="transformer")
+experiment.log_parameters(hyper_params)
+
+# Train the Model
+def train(model, train_loader, experiment, hyperparams):
+    # Loss and Optimizer
+    loss_fn = nn.CrossEntropyLoss(ignore_index=0)
+    optimizer = torch.optim.Adam(model.parameters(), lr=hyper_params["learning_rate"])
+
+    model = model.train()
+    with experiment.train():
+        for e in range(hyper_params['num_epochs']):
+            for batch in tqdm(train_loader):
+                x = batch['input_vectors'].to(device)
+                y = batch['label_vectors'].to(device)
+                lengths = batch['lengths'].to(device)
+                optimizer.zero_grad()
+                y_pred = model(x)
+                y_pred = torch.flatten(y_pred, 0, 1)
+                y_actual = torch.flatten(y, 0, 1)
+                loss = loss_fn(y_pred, y_actual)
+                loss.backward()
+                optimizer.step()
+
+                num_words_in_batch = torch.sum(lengths).item()
+                total_batch_loss = loss.item()*num_words_in_batch
+                perplexity = np.exp(total_batch_loss / num_words_in_batch)
+                experiment.log_metric("perplexity", perplexity)
+
+                num_wrong_in_batch = 0
+                for i in range(len(lengths)):
+                    diff = torch.argmax(y_pred[i, :])[0:lengths[i]] - y[i, 0:lengths[i]]
+                    num_wrong_in_batch += np.count_nonzero(diff.cpu())
+
+                accuracy = 1 - (num_wrong_in_batch / num_words_in_batch)
+                experiment.log_metric("accuracy", accuracy)
+        # Forward + Backward + Optimize
+
+        # Compute train accuracy
+
+        # Log perplexity to Comet.ml using experiment.log_metric
+
+
+# Test the Model
+def test(model, train_loader, experiment, hyperparams):
+    # Loss and Optimizer
+    loss_fn = nn.CrossEntropyLoss(ignore_index=0)
+    optimizer = torch.optim.Adam(model.parameters(), lr=hyper_params["learning_rate"])
+    total_loss = 0
+    word_count = 0
+    total_wrong = 0
+
+    model = model.eval()
+    with experiment.test():
+        for batch in tqdm(test_loader):
+            x = batch['input_vectors'].to(device)
+            y = batch['label_vectors'].to(device)
+            lengths = batch['lengths'].to(device)
+
+            y_pred = model(x)
+            y_pred = torch.flatten(y_pred, 0, 1)
+            y_actual = torch.flatten(y, 0, 1)
+            loss = loss_fn(y_pred, y_actual)
+
+            num_words_in_batch = torch.sum(lengths).item()
+            total_loss += loss.item()*num_words_in_batch
+            word_count += num_words_in_batch
+
+            num_wrong_in_batch = 0
+            for i in range(len(lengths)):
+                diff = torch.argmax(y_pred[i, :])[0:lengths[i]] - y[i, 0:lengths[i]]
+                num_wrong_in_batch += np.count_nonzero(diff.cpu())
+            total_wrong += num_wrong_in_batch
+        
+        perplexity = np.exp(total_loss / word_count)
+        accuracy = 1 - (total_wrong / word_count)
+        experiment.log_metric("perplexity", perplexity)
+        experiment.log_metric("accuracy", accuracy)
+
+        # Log perplexity to Comet.ml using experiment.log_metric
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("train_file")
+    parser.add_argument("test_file")
+    parser.add_argument("-l", "--load", action="store_true",
+                        help="load model.pt")
+    parser.add_argument("-s", "--save", action="store_true",
+                        help="save model.pt")
+    parser.add_argument("-T", "--train", action="store_true",
+                        help="run training loop")
+    parser.add_argument("-t", "--test", action="store_true",
+                        help="run testing loop")
+    args = parser.parse_args()
+    
+    # Data Loader (Input Pipeline)
+    train_loader, test_loader, vocab_size, window_size = load_dataset(args.train_file, args.test_file, hyper_params['batch_size'])
+  
+    # Initialize your transformer using the hyper-parameters
+    model = Transformer(
+        vocab_size,
+        window_size,
+        hyper_params['batch_size'],
+        hyper_params['embedding_size']
+    )
+
+    if args.load:
+        print("loading saved model...")
+        model.load_state_dict(torch.load('./model.pt'))
+    if args.train:
+        print("running training loop...")
+        train(model, train_loader, experiment, hyper_params)
+    if args.train:
+        print("running testing loop...")
+        test(model, test_loader, experiment, hyper_params)
+    if args.save:
+        print("saving model...")
+        torch.save(model.state_dict(), './model.pt')
+

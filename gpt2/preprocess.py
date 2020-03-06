4a212e64179a045b860009a1c29e05181f6035f1
Tiger <tiger.dingsun@gmail.com>
2020-03-06 14:12:50 -0500

    preprocessing test

--- gpt2/preprocess.py
+++ gpt2/preprocess.py
@@ -28,8 +28,8 @@ def load_transformer_dataset(train_fn, test_fn, tokenizer, batch_size):
             input_seq = tokenizer.bos_token + " " + line
             label = line + " " + tokenizer.eos_token
             train_lengths.append(len(input_seq))
-            train_inputs.append(torch.tensor(tokenizer.encode(input_seq, max_length=max_seq_len, pad_to_max_length=True, add_special_tokens=False)))
-            train_labels.append(torch.tensor(tokenizer.encode(label, max_length=max_seq_len, pad_to_max_length=True, add_special_tokens=False)))
+            train_inputs.append(torch.tensor(tokenizer.encode(input_seq, max_length=max_seq_len, pad_to_max_length=True)))
+            train_labels.append(torch.tensor(tokenizer.encode(label, max_length=max_seq_len, pad_to_max_length=True)))
     train_lengths = torch.tensor(train_lengths)
 
     test_inputs = []
@@ -40,8 +40,8 @@ def load_transformer_dataset(train_fn, test_fn, tokenizer, batch_size):
             input_seq = tokenizer.bos_token + " " + line
             label = line + " " + tokenizer.eos_token
             test_lengths.append(len(input_seq))
-            test_inputs.append(torch.tensor(tokenizer.encode(input_seq, max_length=max_seq_len, pad_to_max_length=True, add_special_tokens=False)))
-            test_labels.append(torch.tensor(tokenizer.encode(label, max_length=max_seq_len, pad_to_max_length=True, add_special_tokens=False)))
+            test_inputs.append(torch.tensor(tokenizer.encode(input_seq, max_length=max_seq_len, pad_to_max_length=True)))
+            test_labels.append(torch.tensor(tokenizer.encode(label, max_length=max_seq_len, pad_to_max_length=True)))
     test_lengths = torch.tensor(test_lengths)
 
     train_dataset = TransformerDataset(train_inputs, train_labels, train_lengths)

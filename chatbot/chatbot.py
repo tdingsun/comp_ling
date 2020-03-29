6941396a98ea1a275d06b8d880e3327215730d2e
Tiger <tiger.dingsun@gmail.com>
2020-03-29 18:55:30 -0400

    first push

---
+++ chatbot/chatbot.py
@@ -0,0 +1,162 @@
+from comet_ml import Experiment
+import torch
+import torch.nn as nn
+import argparse
+import math
+import numpy as np
+from preprocess import *
+from tqdm import tqdm
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+hyper_params = {
+     "batch_size": 20,
+     "num_epochs": 10,
+     "learning_rate": 0.003,
+     "window_size": 100
+ }
+
+
+def train(model, train_loader, optimizer, experiment, pad_index):
+    """
+    Trains the model.
+    :param model: the initilized model to use for forward and backward pass
+    :param train_loader: Dataloader of training data
+    :param optimizer: the initilized optimizer
+    :param experiment: comet.ml experiment object
+    """
+    # TODO: Write the training loop here, save trained model weights if needed
+    loss_fn = nn.CrossEntropyLoss(ignore_index=pad_index)
+    model = model.train()
+
+    batch_num = 0
+    with experiment.train():
+        for batch in tqdm(train_loader):
+            x = batch['input_vectors'].to(DEVICE)
+            y = batch['label_vectors'].to(DEVICE)
+            outputs = model(x, labels=x)
+            _, logits = outputs[:2]
+            myLoss = loss_fn(torch.flatten(logits, 0, 1), torch.flatten(y, 0, 1))
+            myLoss.backward()
+            optimizer.step()
+
+            lengths = batch['lengths']
+            num_words_in_batch = torch.sum(lengths).item()
+            total_batch_loss = myLoss.item()*num_words_in_batch
+            perplexity = np.exp(total_batch_loss / num_words_in_batch)
+            experiment.log_metric("perplexity", perplexity)
+            print(perplexity)
+
+
+            if batch_num % 50 == 0:
+                torch.save(model.state_dict(), 'model.pt')
+
+            batch_num += 1
+
+
+def test(model, test_loader, experiment, pad_index):
+    """
+    Validates the model performance as LM on never-seen data using perplexity.
+    :param model: the trained model to use for testing
+    :param test_loader: Dataloader of testing data
+    :param experiment: comet.ml experiment object
+    """
+    total_loss = 0
+    word_count = 0
+    # TODO: Write the testing loop and calculate perplexity
+    loss_fn = nn.CrossEntropyLoss(ignore_index=pad_index)
+    model = model.eval()
+    with experiment.validate():
+        for batch in tqdm(test_loader):
+            x = batch['input_vectors'].to(DEVICE)
+            y = batch['label_vectors'].to(DEVICE)
+            outputs = model(x, labels=x)
+            _, logits = outputs[:2]
+            myLoss = loss_fn(torch.flatten(logits, 0 , 1), torch.flatten(y, 0, 1))
+            myLoss.backward()
+            optimizer.step()
+
+            lengths = batch['lenghts']
+            num_words_in_batch = torch.sum(lengths).item()
+            total_loss += myLoss.item()*num_words_in_batch
+            word_count += num_words_in_batch
+        
+        perplexity = np.exp(total_loss / word_count)
+        print("perplexity:", perplexity)
+        experiment.log_metric("perplexity", perplexity)
+
+
+def interactive(input, tokenizer, model, top_k=10, ntok=20):
+    """
+    Generate and print out the response given input using the trained model
+    :param input: an input string as prompt (i.e. How are you?)
+    :param tokenizer: intialized tokenizer object for encoding the input
+    :param model: the trained model to use for generate prediction
+    :param top_k: number of samples for top_l sampling
+    :param ntok: maximum number of tokens to generate
+
+    Comment: Feed in the input to the model to generate the most probable token
+    and concatenate it with current input.
+    Continue this process iteratively until the model predicts the padding
+    token or reach the maximum number of tokens.
+    You may need to add the BOS token and special token to the input sentence
+    before passing into model.
+    Also, you may want to filter out your input sentence and meaningless tokens
+    when printing out the response.
+    """
+    # TODO: Write the generation function for interacting with trained model
+    response = None
+    print(response)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("train_file")
+    parser.add_argument("test_file")
+    parser.add_argument("-l", "--load", action="store_true",
+                        help="load model.pt")
+    parser.add_argument("-s", "--save", action="store_true",
+                        help="save model.pt")
+    parser.add_argument("-T", "--train", action="store_true",
+                        help="run training loop")
+    parser.add_argument("-t", "--test", action="store_true",
+                        help="run testing loop")
+    parser.add_argument("-i", "--interative", action="store_true",
+                        help="run in interactive mode")
+    args = parser.parse_args()
+
+    experiment = Experiment(log_code=False)
+    experiment.log_parameters(hyper_params)
+
+    # Load the GPT2 Tokenizer, add any special token if needed
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+    special_tokens_dict = {'sep_token': '<SEP>', 'pad_token': '<PAD>'}
+    tokenizer.add_special_tokens(special_tokens_dict)
+    pad_index = tokenizer.pad_token_id
+
+    # Intialized the pretrained GPT-2 model and optimizer
+    model = GPT2LMHeadModel.from_pretrained('gpt2').to(DEVICE)
+    model.resize_token_embeddings(len(tokenizer))
+    optimizer = torch.optim.Adam(model.parameters(), lr=hyper_params["learning_rate"])
+    # Load the train, test DataLoader NOTE: Parse the data using GPT2 tokenizer
+    train_loader, test_loader = load_dataset(args.train_file, args.test_file, tokenizer, hyper_params["batch_size"], hyper_params["window_size"])
+    
+    if args.load:
+        model.load_state_dict(torch.load('model.pt'))
+    if args.train:
+        # run train loop here
+        print("running training loop...")
+        train(model, train_loader, optimizer, experiment, pad_index)
+    if args.save:
+        torch.save(model.state_dict(), 'model.pt')
+    if args.test:
+        # run test loop here
+        print("running testing loop...")
+        test(model, test_loader, experiment, pad_index)
+    if args.interactive:
+        # generate your own chat with the model here
+        print("running interative mode...")
+        while True:
+            input_text = input("Please say something: ")
+            interactive(input_text, tokenizer, model)
---
+++ chatbot/preprocess.py
@@ -0,0 +1,73 @@
+from transformers import *
+from torch.utils.data import Dataset, DataLoader
+import torch
+
+
+def load_dataset(train_fn, test_fn, tokenizer, batch_size, window_size):
+    """
+    :param fn: filename for the dataset
+    :return: (torch.utils.data.DataLoader, torch.utils.data.DataLoader) for
+    train and test
+
+    :Comment: This function should be similary as the last GPT-2 assignemnent.
+    We are still using the GPT-2 tokenizer to get the word ids.
+    One thing to note is that when preprocessing the dataset, please exclude
+    all the sentences defining the speaker's persona, in this assignment,
+    we will just be training and testing on the chat text data. Also for each
+    record of chat in the format of "sentence \t response", add BOS and EOS
+    token to the start and end it, also add a special token between sentence
+    and response to differentiate them from each other.
+    """
+    train_loader = read_files(train_fn, tokenizer, window_size, batch_size)
+    test_loader = read_files(test_fn, tokenizer, window_size, batch_size)
+
+    return train_loader, test_loader
+
+
+def read_files(fn, tokenizer, max_len, batch_size):
+    inputs = []
+    labels = []
+    lengths = []
+
+    with open(fn, 'r') as f:
+        for line in f:
+            #exclude any sentences starting with  your persona: or partner's persona
+            #only sentences wtih sentence tab response
+            #BOS, sentence, special token, repsonse, EOS
+            if " persona: " not in line:
+                #TODO: remove numbers? (sentence ids)
+                sen_res = line.strip().replace('\t', " " + tokenizer.sep_token + " ")
+                inpt = tokenizer.bos_token + " " + sen_res
+                label = sen_res + " " + tokenizer.eos_token
+                encoded_line = tokenizer.encode(inpt, max_length = max_len)
+                lengths.append(len(encoded_line))
+                inputs.append(torch.tensor(tokenizer.encode(inpt, max_length=max_len, pad_to_max_length=True)))
+                labels.append(torch.tensor(tokenizer.encode(label, max_length=max_len, pad_to_max_length=True)))
+
+    dataset = ChatDataset(inputs, labels, lengths)
+    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    return data_loader
+
+
+
+class ChatDataset(Dataset):
+    def __init__(self, inputs, labels, lengths):
+        self.input_vectors = inputs
+        self.label_vectors = labels
+        self.lengths = lengths
+    
+    def __len__(self):
+        return len(self.input_vectors)
+
+    def __getitem__(self, idx):
+        item = {
+            "input_vectors": self.input_vectors[idx],
+            "label_vectors": self.label_vectors[idx],
+            "lengths": self.lengths[idx]
+        }
+        return item
+
+                
+
+
+

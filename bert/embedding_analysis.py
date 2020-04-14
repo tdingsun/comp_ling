0e376bdb1688b150860afbe4620100494400a157
Tiger <tiger.dingsun@gmail.com>
2020-04-13 22:23:26 -0400

    test

--- bert/embedding_analysis.py
+++ bert/embedding_analysis.py
@@ -85,9 +85,9 @@ def embedding_analysis(model, experiment, train_set, test_set, batch_size, word2
                     word_embedding = embedding[position, :]
                     embeddings_for_plot.append(word_embedding)
                     texts.append(word)
-        print(len(embeddings_for_plot))
+        embeddings_for_plot = torch.tensor(embeddings_for_plot)
 
-        plot_embeddings(texts, torch.tensor(embeddings_for_plot).cpu().detach().numpy(), key)
+        plot_embeddings(texts, embeddings_for_plot.cpu().detach().numpy(), key)
 
 
         # TODO: Give these sentences as input, and obtain the specific word

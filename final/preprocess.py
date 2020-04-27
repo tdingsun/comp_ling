9dd3e5ed616fdacc3868ca5a5f5c910ab01a2497
students <students@GD-Loaner-B.fios-router.home>
2020-04-27 18:18:30 -0400

    debug

--- final/preprocess.py
+++ final/preprocess.py
@@ -4,9 +4,6 @@ import numpy as np
 import random
 import math
 
-PAD_TOKEN = '<PAD>'
-MASK_TOKEN = '<MASK>'
-
 class MyDataset(Dataset):
     # TODO: Create masked Penn Treebank dataset.
     #       You can change signature of the initializer.
@@ -17,11 +14,12 @@ class MyDataset(Dataset):
         words_arr = read_file(input_fn) #array of all words
         contents = tokenize(words_arr, char2id, max_word_len) #breaking words into chars
         # Split data into fixed length sequences
-        self.sequences = [contents[:-1][i*window_size:(i+1)*window_size] for i in range(len(contents[:-1]) // window_size)]
+        self.sequences = [contents[i*window_size:(i+1)*window_size] for i in range((len(contents) - 1) // window_size)]
         self.sequences = torch.tensor(self.sequences)
         # sentences = [contents[i:i+window_size] for i in range(len(contents) - window_size)]
         print("time for labels")
-        self.labels = [words_arr[1:][i*window_size:(i+1)*window_size] for i in range(len(contents[1:]) // window_size)]
+        words_arr = words_arr[1:]
+        self.labels = [words_arr[i*window_size:(i+1)*window_size] for i in range((len(contents) - 1) // window_size)]
         # self.labels = [word2id[w] for w in words_arr[1:]] + [word2id[words_arr[-1]]]
         print("make torch")
 

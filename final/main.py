1d6bc134be3e08975c3a740f1153b0a95e3e1dab
students <students@GD-Loaner-B.fios-router.home>
2020-04-28 16:14:17 -0400

    hp change

--- final/main.py
+++ final/main.py
@@ -12,7 +12,7 @@ from tqdm import tqdm  # optional progress bar
 
 # TODO: Set hyperparameters
 hyperparams = {
-    "num_epochs": 10,
+    "num_epochs": 5,
     "lr": 1.0,
     "lstm_batch_size": 20,
     "lstm_seq_len": 35,
